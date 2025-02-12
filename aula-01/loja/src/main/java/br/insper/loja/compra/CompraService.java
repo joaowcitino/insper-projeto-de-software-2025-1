941a067be1d0aafdaec953ee3fe64b09bc366ce3
Joao Citino <joaocitino10@gmail.com>
2025-02-12 11:23:58 -0300

    feat: Add produto service

--- aula-01/loja/src/main/java/br/insper/loja/compra/CompraService.java
+++ aula-01/loja/src/main/java/br/insper/loja/compra/CompraService.java
@@ -1,17 +1,17 @@
 package br.insper.loja.compra;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.insper.loja.produto.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import br.insper.loja.evento.EventoService;
 import br.insper.loja.usuario.Usuario;
 import br.insper.loja.usuario.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import br.insper.loja.produto.ProdutoService;
 
 @Service
 public class CompraService {
@@ -25,8 +25,19 @@ public class CompraService {
     @Autowired
     private EventoService eventoService;
 
+    @Autowired
+    private ProdutoService produtoService;
+
     public Compra salvarCompra(Compra compra) {
         Usuario usuario = usuarioService.getUsuario(compra.getUsuario());
+
+        for (String produtoId: compra.getProdutos()) {
+            Produto produto = produtoService.getProduto(produtoId);
+        }
+
+        for (String produtoId: compra.getProdutos()) {
+            Produto produto = produtoService.diminuirQuantidade(produtoId, 1);
+        }
 
         compra.setNome(usuario.getNome());
         compra.setDataCompra(LocalDateTime.now());
